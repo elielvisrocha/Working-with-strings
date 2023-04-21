9636459637dfaf5f62618977d381e2ea53de7fa7
elielvisrocha <rochaelielvis@gmail.com>
2023-04-21 18:22:48 -0300

    first part

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+const airline = 'TAP Air Portugual';
+
+
+
+// const plane = 'A320';
+
+
+
+// console.log(airline.slice(-2));
+
+
+// const checkMiddleSeat = function (seat) {
+//     const s = seat.slice(-1);
+
+//     if (s === 'B' || s === 'E')
+//         console.log(`You got the middle seat!`);
+//     else
+//         console.log(`You got lucky`);
+// }
+
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B737'.length);
+
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Air'));
+
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline);
+
+// console.log(airline.slice(0, airline.indexOf(' ')));

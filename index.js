fbc7644ed30a5be4b730c3b3690a2500d398b3aa
elielvisrocha <rochaelielvis@gmail.com>
2023-04-22 18:36:00 -0300

    second part

--- index.js
+++ index.js
@@ -1,7 +1,70 @@
 'use strict';
 
+// Fix capitalization in name
 
 const airline = 'TAP Air Portugual';
+
+console.log(airline.toLowerCase());
+
+const myName = 'eLvIs';
+
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1, 5).toLowerCase());
+
+
+// Comparing emails
+
+const email = 'hello@elvis.io';
+const loginEmail = ' Hello@Elvis.Io \n';
+
+
+const lowerEmail = loginEmail.toLocaleLowerCase().trim();
+
+console.log(lowerEmail === email);
+
+
+//replacing
+const priceUS = '288,97$';
+const priceBR = priceUS.replace('$', 'R$');
+
+console.log(priceBR);
+
+
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace(/door/g, 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+
+
+// Booleans
+
+const plane = 'A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.startsWith('A32'));
+
+
+plane.startsWith('A320') && plane.endsWith('neo') ?
+    console.log(`Do part of new family`) :
+    console.log(`Don't be part of new family`);
+
+
+//PRACTICE EXERCISE
+const checkBaggage = function (items) {
+    const baggage = items.toLowerCase();
+
+    baggage.includes('knife') || baggage.includes('gun') ? 
+        console.log('You aren\'t allowed on board') :
+        console.log('Welcome aboard');
+}
+
+checkBaggage(`I've a laptop, some food and a pocket knife`);
+checkBaggage(`Socks and camera`);
+checkBaggage(`Got some snacks and a gun for protection`);
+
+
+
+
+
 
 
 
